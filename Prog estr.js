52ada97a7e5e8de06a430068bebf6a16c4d856e6
Frank28sjc <frankcrist22@gmail.com>
2021-08-09 18:34:04 -0300

    Modificçaões de novos arquivos

---
+++ Prog estr.js
@@ -0,0 +1,12 @@
+n1 = prompt("Digite primeiro valor")
+n2 = prompt ("Digite segundo valor")
+
+if (n1 > n2){
+document.write('O primero numero informado é maior');
+}
+else if (n1 == n2){
+document.write('Ambos numero são iguais');
+}
+else{
+document.write('O segundo numero é maior');
+}
--- main.js
+++ main.js
@@ -1,2 +1,60 @@
-var nome = ("CRISTOPHER AGUILAR")
-alert("Seja bemvindo"+ nome)
+window.onload = function () {
+  
+  var seconds = 00; 
+  var tens = 00; 
+  var appendTens = document.getElementById("decimos")
+  var appendSeconds = document.getElementById("segundos")
+  var buttonStart = document.getElementById('button-start');
+  var buttonStop = document.getElementById('button-stop');
+  var buttonReset = document.getElementById('button-reset');
+  var Interval ;
+
+  buttonStart.onclick = function() {
+    
+     clearInterval(Interval);
+     Interval = setInterval(startTimer, 10);
+  }
+  
+    buttonStop.onclick = function() {
+       clearInterval(Interval);
+  }
+  
+
+  buttonReset.onclick = function() {
+     clearInterval(Interval);
+    decimos = "00";
+  	segundos = "00";
+    appendTens.innerHTML = decimos;
+  	appendSeconds.innerHTML = segundos;
+  }
+  
+   
+  
+  function startTimer () {
+    decimos++; 
+    
+    if(decimos <= 9){
+      appendTens.innerHTML = "0" + decimos;
+    }
+    
+    if (decimos > 9){
+      appendTens.innerHTML = decimos;
+      
+    } 
+    
+    if (decimos > 99) {
+      console.log("segundos");
+      segundoss++;
+      appendSeconds.innerHTML = "0" + segundos;
+      decimos = 0;
+      appendTens.innerHTML = "0" + 0;
+    }
+    
+    if (segundos > 9){
+      appendSeconds.innerHTML = segundos;
+    }
+  
+  }
+  
+
+}
